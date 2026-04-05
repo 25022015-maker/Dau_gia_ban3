15bfea0716f416e5e9f13e67ae18e1ad9cbc2606
Pham Thi Thanh Thao <25022015@vnu.edu.vn>
2026-04-06 00:02:07 +0700

    new created

---
+++ src/main/java/entitiesclasses/Entity.java
@@ -0,0 +1,16 @@
+package entitiesclasses;
+import java.time.LocalDateTime;
+
+public abstract class Entity {
+    //attributes
+    private static int nextId = 1;
+    private final int id;
+    private final LocalDateTime createdAt;
+    //constructor
+    public Entity(){
+        this.id = nextId++;
+        this.createdAt = LocalDateTime.now();
+    }
+    public int getId(){return id;}
+    public LocalDateTime getTime(){return createdAt;}
+}
