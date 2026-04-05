5cb02b41c31a69a8542ad2baf4e617603312aede
Pham Thi Thanh Thao <25022015@vnu.edu.vn>
2026-04-05 23:42:44 +0700

    new created

---
+++ src/main/java/com/example/dau_gia_ban3/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.dau_gia_ban3;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
