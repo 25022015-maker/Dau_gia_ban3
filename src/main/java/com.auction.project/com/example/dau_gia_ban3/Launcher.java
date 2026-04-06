922ef329392dbb822fe9e1fbeb05dadf5af2a92c
Pham Thi Thanh Thao <25022015@vnu.edu.vn>
2026-04-06 12:18:56 +0700

    Dọn dẹp triệt để: Chỉ giữ lại cấu trúc com.auction.project

--- src/main/java/com.auction.project/com/example/dau_gia_ban3/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.auction.project.com.example.dau_gia_ban3;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
