2c83954bda40a5d4fb93518692d9228deedb7559
nucu <sieuxin123@gmail.com>
2026-04-12 06:33:25 +0700

    Seller Class

---
+++ src/main/java/com/auction/project/entitiesclasses/Seller.java
@@ -0,0 +1,36 @@
+package com.auction.project.entitiesclasses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Seller extends User {
+    private List<String> listedItems;
+    private double rating;
+
+    public Seller(String username, String password, String email) {
+        super(username, password, email);
+        this.listedItems = new ArrayList<>();
+        this.rating = 0.0;
+    }
+
+    public void createAuction(String itemName) {
+        if (isLogin()) {
+            listedItems.add(itemName);
+            System.out.println("Nguoi ban hang  " + getUsername() + " len hang thanh cong: " + itemName);
+        } else {
+            System.out.println("Phai dang nhap");
+        }
+    }
+
+    public List<String> getListedItems() {
+        return listedItems;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
