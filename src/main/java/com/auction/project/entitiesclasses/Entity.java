7a8f5495b597f96d600caa85f136a29009cb406d
Pham Thi Thanh Thao <25022015@vnu.edu.vn>
2026-04-11 17:34:18 +0700

    generics cho getInfo()

--- src/main/java/com/auction/project/entitiesclasses/Entity.java
+++ src/main/java/com/auction/project/entitiesclasses/Entity.java
@@ -14,5 +14,5 @@ public abstract class Entity {
 
     public int getId(){return id;}
     public LocalDateTime getTime(){return createdAt;}
-    public abstract String getInfo();
+    public abstract T getInfo<T>();
 }
