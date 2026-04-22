646a9db4a750ad548b16cf441a43fbb5d73186e3
Calcifer <25022026@vnu.edu.vn>
2026-04-23 04:55:53 +0700

    màn hình đăng nhập: enter để chuyển tiếp sang ô text khác, enter = login, thông báo sai acc, gõlaijij để reset thông báo, sidebar popup,...

---
+++ src/main/java/com/example/uinew/License.java
@@ -0,0 +1,16 @@
+package com.example.uinew;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class License extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
