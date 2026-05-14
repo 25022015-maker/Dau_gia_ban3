fc6e186f1eff62581041f45563cafbd90f5f44c7
Calcifer <25022026@vnu.edu.vn>
2026-05-14 15:07:56 +0700

    update

--- src/main/java/com/example/uinew/Controller/CreateBiddingController.java
+++ src/main/java/com/example/uinew/Controller/CreateBiddingController.java
@@ -4,6 +4,7 @@ import com.example.uinew.model.Product;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.paint.Color;
 
 import java.time.LocalDate;
 
@@ -13,41 +14,66 @@ public class CreateBiddingController {
     @FXML
     private DatePicker end;
     @FXML
+    private Spinner<Integer> hourStart, minStart, hourEnd, minEnd;
+    @FXML
     private DatePicker start;
     @FXML
     Button cancel;
 
-   // @FXML
-   // Label lblError;
+   @FXML
+   Label lblError;
 
 
     @FXML
     void handleCreate(ActionEvent event) {
 
+        lblError.setText("");
+        lblError.setTextFill(Color.RED);
+
         String name = txtName.getText();
+        String description = txtDescription.getText();
+        String productType = choiceBox.getValue();
         double startPrice = 0;
         Boolean hasError = false;
+        LocalDate dateEnd = end.getValue();
 
-        if (txtStartPrice == null || txtStartPrice.getText().trim().isEmpty()) {
+        Integer hS = hourStart.getValue();
+        Integer mS = minStart.getValue();
+
+        LocalDate dateStart = start.getValue();
+
+        Integer hE = hourEnd.getValue();
+        Integer mE = minEnd.getValue();
+
+        if (txtStartPrice.getText().trim().isEmpty()) {
             //lblError.isVisible();
-            System.out.println("Vui lon nhập giá tiền");
+            showError("Lỗi: Vui lòng nhập giá khởi điểm!");
+            System.out.println("Vui long nhập giá tiền");
         } else {
             try {
                 startPrice = Double.parseDouble(txtStartPrice.getText());
 
             } catch (NumberFormatException e) {
                 System.out.println("Lỗi: Giá tiền phải là một con số hợp lệ!");
+                showError("Lỗi: Giá tiền phải là số dương hợp lệ!");
                 hasError = true;
             }
         }
 
-        LocalDate dateEnd = end.getValue();
-        LocalDate dateStart = start.getValue();
-        String description = txtDescription.getText();
-        String productType = choiceBox.getValue();
-
-        if (productType == null) {
+        if (dateStart == null || dateEnd == null) {
+            System.out.println("Vui lòng chọn đầy đủ ngày bắt đầu và kết thúc");
+            showError("Lỗi: Vui lòng chọn cả ngày bắt đầu và kết thúc!");
             hasError = true;
+        }
+
+        if (productType.isEmpty()) {
+            showError("Lỗi: Vui lon chọn loại sản phẩm");
+            hasError = true;
+        }
+
+        if (name.isEmpty()) {
+            showError("Lỗi: Tên sản phẩm không được để trống!");
+            return;
         }
 
         if (!hasError) {
@@ -60,6 +86,7 @@ public class CreateBiddingController {
             MainController.setSelectedProduct(newProduct);
 
             System.out.println("Tạo phiên đấu giá thành công!");
+            showError("Tạo phiên đấu giá thành công!");
 
             HomeController.getInstance().setView("/com/example/uinew/CurrentBiddingView/ThisBidding.fxml");
         }
@@ -69,6 +96,10 @@ public class CreateBiddingController {
     @FXML
     public void backToDashboard() {
         HomeController.getInstance().setView("/com/example/uinew/Dashboard.fxml");
+    }
+
+    private void showError(String message) {
+        lblError.setText(message);
     }
 
     @FXML
@@ -83,9 +114,15 @@ public class CreateBiddingController {
             // oldValue : giá trị trước khi đổi
             // newValue : giá trị mới client vừa chọn
 
+
             System.out.println("Client vừa đổi từ: " + oldValue + " sang " + newValue);
             String productType = newValue;    //gán newValue vào một biến toàn cục để lưu trữ
         });
+        hourStart.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23, 0));
+        minStart.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59, 0));
+
+        hourEnd.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23, 23));
+        minEnd.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59, 59));
     }
 
 }
